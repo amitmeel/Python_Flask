44c85fc4248d0c759bb6b2d1836aeef7fdb461b2
Amit Meel <amit.amitmeel.meel@gmail.com>
2019-09-08 22:01:08 +0530

    added new comments to intro flask routes

--- Routes/app.py
+++ Routes/app.py
@@ -2,15 +2,15 @@ from flask import Flask , jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/')  # 'GET' request only
 def index():
     return '<h1>Hello, World!</h1>'
 
-@app.route('/home')
+@app.route('/home')  # GET request only
 def home():
     return '<h1>You are on the home</h1>'
 
-@app.route('/json')
+@app.route('/json') # Git request only
 def json():
     return jsonify({'key':'value', 'key2':[123,4345]})
 
